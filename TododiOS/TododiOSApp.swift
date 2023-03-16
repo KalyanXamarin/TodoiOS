a33492462c77a27a4d3e6e98a4b29d6756464130
Kalyan Pidugu <pidugukalyan@gmail.com>
2023-03-16 05:46:04 +0530

    Working iOS project

---
+++ TododiOS/TododiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TododiOSApp.swift
+//  TododiOS
+//
+//  Created by kalyan Pidugu on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TododiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
